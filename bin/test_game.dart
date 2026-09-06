c77d0c21660b269ac7068683d7ac8087547b3d6d
Sebastian Ladwig <seb.ladwig@gmail.com>
2026-09-06 09:54:23 +0200

    Hexagons get distributed randomly on Gameboard

--- bin/test_game.dart
+++ bin/test_game.dart
@@ -2,5 +2,6 @@ import 'package:game_of_hexia/src/models/hexagon_board.dart';
 
 void main() {
   HexagonBoard board = HexagonBoard();
+  board.placeHexagons();
   print(board);
 }
--- lib/src/models/hexagon_board.dart
+++ lib/src/models/hexagon_board.dart
@@ -3,6 +3,7 @@
 class HexagonBoard {
   final Map<({int r, int q}), Hexagon> _grid = {};
 
+  //Each tile/hexagon has 6 different directions/borders
   final directions = [
        (0, -1),   (1, -1),
     (-1, 0),/* Hexa */ (1, 0), 
@@ -14,19 +15,38 @@ class HexagonBoard {
     int q = 2;
     for(int r = 0; r < 3; r++) {
       for(int i = q; i < 5; i++) {
-        _grid[(r: r, q: i)] = Hexagon.empty;
+        _grid[(r: r, q: i)] = Hexagon(type: HexagonType.empty, owner: PlayerType.none);
       }
       q--;
     }
-
     //q as limit in the grid. the last row is (6,0), (6,1), (6,2)
     q = 4;
     for(int r = 3; r < 5; r++) {
       for(int i = 0; i < q; i++) {
-        _grid[(r: r, q: i)] = Hexagon.empty;
+        _grid[(r: r, q: i)] = Hexagon(type: HexagonType.empty, owner: PlayerType.none);
       }
       q--;
     }
+  }
+
+  //Use this to place hexagons randomly on board
+  void placeHexagons() {
+    //Number of different tiles on gameboard. e.g. there are 3 stone tiles
+    List<HexagonType> numberHexTypes = [
+    HexagonType.stone, HexagonType.stone, HexagonType.stone,
+    HexagonType.wood, HexagonType.wood, HexagonType.wood, HexagonType.wood, 
+    HexagonType.cattle, HexagonType.cattle, HexagonType.cattle, HexagonType.cattle,
+    HexagonType.fish, HexagonType.fish, HexagonType.fish, HexagonType.fish,
+    HexagonType.iron, HexagonType.iron, HexagonType.iron,
+    HexagonType.empty];
+    
+    numberHexTypes.shuffle();
+
+    var i = 0;
+      _grid.forEach((_, value) {
+        value.type = numberHexTypes[i];
+        i++;
+      });
   }
 
   //Returns the (r, q) Coords of Neighbour
@@ -37,14 +57,14 @@ class HexagonBoard {
   //Returns Size of the Map
   int get length => _grid.length;
 
+  //Using the same Loop as the Constructor, but this time prints
   @override
   String toString() {
-    //this is the same loop as in the constructor. just to see if its working
     final buffer = StringBuffer();
     int q = 2;
     for(int r = 0; r < 3; r++) {
       for(int i = q; i < 5; i++) {
-        buffer.write('[ ]');
+        buffer.write('[${_grid[(r: r,q: i)]?.type.name}]');
       }
       q--;
       buffer.writeln();
@@ -52,7 +72,7 @@ class HexagonBoard {
     q = 4;
     for(int r = 3; r < 5; r++) {
       for(int i = 0; i < q; i++) {
-        buffer.write('[ ]');
+        buffer.write('[${_grid[(r: r,q: i)]?.type.name}]');
       }
       q--;
       buffer.writeln();
@@ -61,15 +81,31 @@ class HexagonBoard {
   }
 }
 
-enum Hexagon {
-  stone(name: "Stone", number: 3),
-  wood(name: "Wood", number: 4),
-  cattle(name: "Cattle", number: 4),
-  fish(name: "Fish", number: 4),
-  iron(name: "Iron", number: 3),
-  empty(name: "Empty", number: 1);
+enum HexagonType {
+  stone(name: "Stone"),
+  wood(name: "Wood"),
+  cattle(name: "Cattle"),
+  fish(name: "Fish"),
+  iron(name: "Iron"),
+  empty(name: "Empty");
 
   final String name;
-  final int number;
-  const Hexagon({required this.name, required this.number});
+  const HexagonType({required this.name});
+}
+
+//The game tile
+class Hexagon {
+  Hexagon({required this.type, required this._owner, this._numberDisc});
+  HexagonType type;
+  PlayerType _owner;
+  int? _numberDisc;
+
+
+}
+
+//Goes on the Hexagons. There is a way to dsitribute them in the game guide. Otherwise could distribute random
+const List<int> numberDiscs = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12];
+
+enum PlayerType {
+  blue, red, yellow, green, none;
 }
